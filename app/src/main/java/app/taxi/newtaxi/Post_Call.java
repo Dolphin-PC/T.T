eabcb89e816cd9638da38acae6e5d2897e49e3c9
pcy <pcx474@gmail.com>
2019-05-04 22:25:28 +0900

    Post_call 텍스트 여부에 따라 맵/채팅 버튼 변경 구현

--- app/src/main/java/app/taxi/newtaxi/Post_Call.java
+++ app/src/main/java/app/taxi/newtaxi/Post_Call.java
@@ -1,17 +1,29 @@
 package app.taxi.newtaxi;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,12 +41,16 @@ public class Post_Call extends AppCompatActivity {
     DatabaseReference mDatabaseMSG,mDatabase;
     ListView COMMENTlist;
     EditText COMMENTedit;
-    Button COMMENTbutton;
     String ID,INDEX,PROFILEURL;
     Date today = new Date();
     SimpleDateFormat timeNow = new SimpleDateFormat("a K:mm");
     String Time = timeNow.format(today);
     StringBuffer SB;
+    Dialog dialog;
+    MapView mMapView;
+    TextView TIMEtext,PRICEtext,DISTANCEtext;
+    Button JOINbutton,MAPbutton,COMMENTbutton;
+    String SELECT_latitude = "37.566643",SELECT_longitude = "126.978279";
     void init(){
         SharedPreferences positionDATA = getSharedPreferences("positionDATA",MODE_PRIVATE);
         SharedPreferences.Editor editor = positionDATA.edit();
@@ -42,6 +58,7 @@ public class Post_Call extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         COMMENTbutton = findViewById(R.id.COMMENTbutton);
+        MAPbutton = findViewById(R.id.MAPbutton);
         COMMENTedit = findViewById(R.id.COMMENTedit);
         COMMENTlist = findViewById(R.id.MESSAGElist);
 
@@ -54,8 +71,31 @@ public class Post_Call extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post__call);
         init();
+
         final ChatAdapter adapter = new ChatAdapter(getApplicationContext(), R.layout.comment_listview,list,ID);
         COMMENTlist.setAdapter(adapter);
+        COMMENTedit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(COMMENTedit.getText().toString().equals("")) {
+                    COMMENTbutton.setVisibility(View.INVISIBLE);
+                    MAPbutton.setVisibility(View.VISIBLE);
+                } else {
+                    COMMENTbutton.setVisibility(View.VISIBLE);
+                    MAPbutton.setVisibility(View.INVISIBLE);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        MAPbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DIALOG();
+            }
+        });
         COMMENTbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,7 +109,6 @@ public class Post_Call extends AppCompatActivity {
                     mDatabaseMSG.push().setValue(new Data_message(INDEX,PROFILEURL, ID, SB.toString(), Time));
                     COMMENTedit.setText("");
                 }
-
             }
         });
         Query query = mDatabaseMSG.orderByChild("index").equalTo(INDEX);
@@ -89,6 +128,58 @@ public class Post_Call extends AppCompatActivity {
             public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) { }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+    }
+    void DIALOG(){
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.custom_dialog);
+        dialog.show();
+        mMapView = dialog.findViewById(R.id.MAP_Dialog);
+        TIMEtext = dialog.findViewById(R.id.TIMEtext);
+        PRICEtext = dialog.findViewById(R.id.PRICEtext);
+        DISTANCEtext = dialog.findViewById(R.id.DISTANCEtext);
+        JOINbutton = dialog.findViewById(R.id.JOINbutton);
+        JOINbutton.setVisibility(View.INVISIBLE);
+
+        Query query1 = mDatabase.child("post").orderByChild("index").equalTo(INDEX);
+        query1.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot appleSnapshot : dataSnapshot.getChildren()) {
+                    Data_Post data_post = appleSnapshot.getValue(Data_Post.class);
+                    TIMEtext.setText(data_post.getTime());
+                    PRICEtext.setText(data_post.getPoint());
+                    DISTANCEtext.setText(data_post.getDistance().split(":")[1]);
+                    SELECT_latitude = data_post.getStart_Latitude();
+                    SELECT_longitude = data_post.getStart_Longitude();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        try{
+            Thread.sleep(500);     // DB에서 받아오는 시간 지연 -> 로딩(원돌아가는거)로 변경하기
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        MapsInitializer.initialize(this);
+
+        mMapView.onCreate(dialog.onSaveInstanceState());
+        mMapView.onResume();// needed to get the map to display immediately
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                LatLng latLng = new LatLng(Double.valueOf(SELECT_latitude),Double.valueOf(SELECT_longitude));
+                googleMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("출발 위치"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                googleMap.getUiSettings().setZoomControlsEnabled(true);
+                googleMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+            }
         });
     }
 }
